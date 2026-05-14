462b4643d096a5cfb5845e8299e7ac72af8f29a6
owen477 <3067824067@qq.com>
2026-05-15 02:58:12 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["localhost", "picsum.photos"],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+ output: "standalone",
+ images: {
+ domains: ["localhost", "picsum.photos"],
+ },
+ typescript: {
+ ignoreBuildErrors: true,
+ },
+ eslint: {
+ ignoreDuringBuilds: true,
+ },
 };
 
 export default nextConfig;
